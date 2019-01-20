d7cb3487a682e2c3aa960044f91440b023bc40a2
Sravya Balasa <sravya.balasa@gmail.com>
2019-01-20 05:37:03 -0800

    Website Development
    Frontend Development of the Website

---
+++ real-time-object-detection/frontend/frontend.js
@@ -0,0 +1,6 @@
+var selector = '.classname';
+$(selector).on('click', function(){
+    $(selector).removeClass('classname');
+});
+
+//https://www.digitalocean.com/community/tutorials/an-introduction-to-jquery
