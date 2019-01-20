5e5ae0a59617987a6aacc9e64f5ae2ffd2494895
Sravya Balasa <sravya.balasa@gmail.com>
2019-01-20 06:37:11 -0800

    Media Updates
    Image and Video Demo of website (Beta)

--- real-time-object-detection/frontend/frontend.js
+++ real-time-object-detection/frontend/frontend.js
@@ -1,6 +1,13 @@
-var selector = '.classname';
-$(selector).on('click', function(){
-    $(selector).removeClass('classname');
+//var selector = '.classname';
+//$(selector).on('click', function(){
+   //$(selector).removeClass('classname');
+//});
+
+$(function(){
+	$('.loading').click(function(){
+  		$('.loading').fadeOut();
+  		$('.loading').removeClass();
+	});
 });
 
 //https://www.digitalocean.com/community/tutorials/an-introduction-to-jquery
